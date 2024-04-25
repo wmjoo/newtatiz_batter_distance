98a359dd8e690d0b191b5b895947d3f41ed7aebc
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-04-26 00:53:43 +0900

    Update 240425_newttiz_batter_similarity_calc.py
    st.error('This is an error', icon="🚨")

--- 240425_newttiz_batter_similarity_calc.py
+++ 240425_newttiz_batter_similarity_calc.py
@@ -6,7 +6,9 @@ import requests
 import lxml
 
 input_player = st.text_input('Player Name : ', '')
+st.error('This is an error', icon="🚨")
 # st.write('The selected Player : ', player_name)
+
 
 # URL 설정
 url = 'https://statiz.sporki.com/stats/?m=total&m2=batting&m3=default&so=&ob=&sy=1982&ey=2024&te=&po=&lt=10100&reg=C3000&pe=&ds=&de=&we=&hr=&ha=&ct=&st=&vp=&bo=&pt=&pp=&ii=&vc=&um=&oo=&rr=&sc=&bc=&ba=&li=&as=&ae=&pl=&gc=&lr=&pr=10000&ph=&hs=&us=&na=&ls=1&sf1=PA&sk1=100&sv1=&sf2=G&sk2=&sv2='
